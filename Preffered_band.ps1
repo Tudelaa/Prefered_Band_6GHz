83c1057f38a42f577fbb5424fb3289a2d4a90145
Antonio Tudela Botella <108870102+Tudelaa@users.noreply.github.com>
2025-03-07 16:06:00 +0100

    Add files via upload

---
+++ Preffered_band.ps1
@@ -0,0 +1,41 @@
+﻿# this script is use as remediation to change the prefered band to 6Ghz for realtek and intel Wi-Fi network cards
+
+
+
+
+$Network_values = Get-NetAdapterAdvancedProperty -Name W*i -DisplayName "Preferred Band" -ErrorAction SilentlyContinue
+$Network_Card = Get-NetAdapter -Name Wi* | Select-Object * -ErrorAction SilentlyContinue
+ 
+
+try {
+
+if ($Network_Card.DriverDescription -match "Realtek") { 
+
+                                                       Set-NetAdapterAdvancedProperty -Name W*i -DisplayName "Preferred Band" -DisplayValue "4. 6G first"
+                                                       
+                                                      } 
+
+if ($Network_Card.DriverDescription -match "Intel") { 
+
+                                                       Set-NetAdapterAdvancedProperty -Name W*i -DisplayName "Preferred Band" -DisplayValue "4. Prefer 6GHz band"
+                                                       
+     
+     
+                                                       } 
+
+Write-Host "Preffered Band has changed to 6GHz"
+  
+exit 0
+  
+   }                                                  
+      
+catch {
+
+
+       Write-Host "Preffered Band has not changed to 6GHz"
+       
+       exit 1
+
+       }
+       
+         
---
+++ Wifi6E_detection.ps1
@@ -0,0 +1,64 @@
+﻿
+# This script check if your network card support WiFi 6E checking Driver description and if so
+# check if preferend band is in 6GHz.
+
+# Writte: Antonio Tudela 
+
+
+
+[int]$WiFi_E6_True=0
+
+$Interfaces = Get-NetAdapter | Select-Object * 
+
+
+
+
+foreach ($Interface in $Interfaces) { 
+
+                                         
+
+                                         $Network_Name = $Interface.Name
+                                         $Network_Card = Get-NetAdapter -Name $Network_Name | Select-Object * -ErrorAction SilentlyContinue
+                                         
+                                                                         
+                                         if ($interface.DriverDescription -match "6E" -or $interface.DriverDescription -match "7" ) { 
+                                         write-host "$Network_Name support Wi-Fi 6E"
+                                         
+                                         $Preffered_Band = Get-NetAdapterAdvancedProperty -Name $Network_Name -DisplayName "Preferred Band"
+
+                                                                                                                                     if ($Preffered_Band.DisplayValue -notmatch "4. 6G first" -or $Preffered_Band.DisplayValue -notmatch "4. Prefer 6GHz band"){
+                                                                                                                                     
+                                         
+                                                                                                                                      
+                                                                                                                                      
+                                                                                                                                      $WiFi_E6_True++ 
+                                                                                                                                      
+                                                                                                                                      write-host "Preffered Band is not running 6GHz band"
+                                                                                                                                      
+                                                                                                                                      }
+                                         }
+
+                                         else { 
+
+                                         Write-Host "$Network_Name does not support Wi-Fi 6E"
+                                         
+
+                                              }
+                                         }
+ 
+if($WiFi_E6_True -gt 0) { 
+
+                         exit 1
+                         
+                        }
+                        
+else { 
+
+      exit 0
+      
+      
+     }
+     
+     
+                                                        
+
